6992c5149486f9f8b9e5038b0b4307ec47add061
daovietanh190499 <daovietanhsky@gmail.com>
2023-04-25 17:48:40 +0000

    change folder name

--- fileserver/controller.py
+++ fileserver/controller.py
@@ -1,6 +1,6 @@
 import os
 
 def create_folder(config):
-    if not os.path.isdir("/mnt/" + f"nas{config['file_server_index']}/" + config['username'] ):
-        os.mkdir("/mnt/" + f"nas{config['file_server_index']}/" + config['username'] )
+    if not os.path.isdir("/mnt/" + f"nas{config['file_server_index']}/dohub-" + config['username'] ):
+        os.mkdir("/mnt/" + f"nas{config['file_server_index']}/dohub-" + config['username'] )
     return
