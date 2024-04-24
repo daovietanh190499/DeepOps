73c64cc44a72254b78a6074ef3cee562424ff7d8
daovietanh190499 <daovietanhsky@gmail.com>
2024-04-24 12:42:26 +0000

    dev/ add value.yaml

--- spawners/k8s/dohub-helm-chart/deploy.sh
+++ spawners/k8s/dohub-helm-chart/deploy.sh
@@ -1,7 +1,7 @@
 helm install \
     --set global.imageRegistry="daovietanh99" \
-    --set global.caCrt= ""\
-    --set global.caKey= ""\
+    --set global.caCrt= "" \
+    --set global.caKey= "" \
     --set dohub.replicas=1 \
     --set dohub.image="dohub" \
     --set dohub.githubClientId="abc" \
@@ -9,4 +9,4 @@ helm install \
     --set dohub.adminUsers="daovietanh190499" \
     --set dohub.defaultPort=8443 \
     --set dohub.defaultSpawner="k8s" \
-    vkist dohub-helm-chart/
+    vkist dohub-helm-chart
