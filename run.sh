bec2ddfffedd2cc5de8c97b3a24b3f086441c012
daovietanh190499 <daovietanhsky@gmail.com>
2024-05-05 11:12:27 +0000

    update/clear old replicas

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 helm upgrade -i --create-namespace -n dohub vkist spawners/k8s/dohub --reset-values --values spawners/k8s/dohub/values.yaml
 kubectl rollout restart deploy -n dohub
+kubectl delete replicaset $(kubectl get replicaset -n dohub -o jsonpath='{.items[?(@.spec.replicas==0)].metadata.name }') -n dohub
